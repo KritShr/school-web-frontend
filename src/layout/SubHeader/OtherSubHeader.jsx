d169e7938de6ca61c088e01601fac45676b04145
kwak <sal091625@gmail.com>
2024-08-09 17:27:07 +0900

    fix: OtherSubHeader tailwindcss로 변경

--- src/layout/SubHeader/OtherSubHeader.jsx
+++ src/layout/SubHeader/OtherSubHeader.jsx
@@ -1,26 +1,14 @@
 import PropTypes from "prop-types";
-import styles from "../../css/layout/SubHeader.module.css";
+import styles from "../../css/page/Contact.module.css";
 
 const OtherSubHeader = ({mainTitle, subTitle}) => {
   const subTitleParts = subTitle ? subTitle.split(',') : [];
   return (
-    <div className={[styles.subHeader, ""].join(" ")}>
-      <img className={styles.greenBox} alt="" src="/image2.svg" />
-      <h1 className={styles.title}>
-        {mainTitle}
-      </h1>
-      <div className={styles.subHeaderPath}>
-          <a className={styles.path}>{subTitleParts[0]}</a>
-          <div className={styles.homeButtonIcon}>
-            <img
-              className={styles.pathIcon}
-              loading="lazy"
-              alt=""
-              src="/vector-13.svg"
-            />
-          </div>
-          <div className={styles.path}>{subTitleParts[1]}</div>
-        </div>
+    <div className="w-full -bg--medium text-center py-16">
+      <div className={styles.contact4}>
+        <h1 className="font-semibold text-6xl -text--default-white mt-3 mb-5" >{mainTitle}</h1>
+        <h2 className="font-medium text-2xl -text--default-white mb-5">{subTitleParts[0] +' > '+ subTitleParts[1]} </h2>
+      </div>
     </div>
   );
 };
