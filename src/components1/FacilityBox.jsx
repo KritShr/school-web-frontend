1fc73ac627f28b0c9808cef0aea53ab1c3afc14f
kwak <sal091625@gmail.com>
2024-08-10 23:15:17 +0545

    fix: form 형태 제거

--- src/components1/FacilityBox.jsx
+++ src/components1/FacilityBox.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-const LoginBox = () => {
+const FacilityBox = () => {
+  const moveTo = () =>{
+    //To do: facility detail로 이동 
+  }
   return (
     <div className="p-10 m-10 bg-white rounded-xl shadow-2xl">
       <h1 className="text-3xl font-semibold text-center">
@@ -18,18 +21,13 @@ const LoginBox = () => {
         </div>
       </div>
 
-      <form className="mt-6">
       <div className="mt-6 flex justify-center"> {/* 부모 요소에 중앙 정렬 적용 */}
-          <button
-            type="submit"
-            className="-bg--color-silver test-2xl font-bold w-96 bg-black text-white px-4 py-2 items-center rounded-md hover:-bg--medium duration-200 text-base"
-          >
-            more
-          </button>
-        </div>
-      </form>
+        <button className="-bg--color-silver test-2xl font-bold w-96 bg-black text-white px-4 py-2 items-center rounded-md hover:-bg--medium duration-200 text-base" onClick={moveTo}>
+          more
+        </button>
+      </div>
     </div>
   );
 };
 
-export default LoginBox;
+export default FacilityBox;
