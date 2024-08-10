cef7485348502d461ed965af38500377dfe0cb33
ha11yeon <jaung7207@gmail.com>
2024-08-10 23:49:02 +0545

    퍼실리티 디테일 페이지 추가

--- src/components1/FacilityBox.jsx
+++ src/components1/FacilityBox.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";  // useNavigate를 import
 
 const FacilityBox = () => {
-  const moveTo = () =>{
-    //To do: facility detail로 이동 
-  }
+  const navigate = useNavigate();  // useNavigate 훅 사용
+
+  const moveTo = () => {
+    navigate('/facility-detail');  
+  };
+
   return (
     <div className="p-10 m-10 bg-white rounded-xl shadow-2xl">
       <h1 className="text-3xl font-semibold text-center">
@@ -22,7 +26,12 @@ const FacilityBox = () => {
       </div>
 
       <div className="mt-6 flex justify-center"> {/* 부모 요소에 중앙 정렬 적용 */}
-        <button className="-bg--color-silver test-2xl font-bold w-96 bg-black text-white px-4 py-2 items-center rounded-md hover:-bg--medium duration-200 text-base" onClick={moveTo}>
+        <button 
+          className="-bg--color-silver text-2xl font-bold w-96 bg-black text-white px-4 py-2 items-center rounded-md 
+                    hover:-bg--medium duration-200 text-base" 
+          onClick={moveTo}
+          
+        >
           more
         </button>
       </div>
