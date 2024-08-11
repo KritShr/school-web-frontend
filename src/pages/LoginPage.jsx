6257b4283dbd6fbc723ae084739ce5860a1143ef
kwak <sal091625@gmail.com>
2024-08-11 09:09:22 +0545

    feat: login Db 연결, todo-token을 localStorage에 저장하기

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from "../css/page/Contact.module.css";
+import { toast } from 'react-toastify';
+import axiosInstance from "../utils/axios";
 
 const LoginPage = () => {
-    const handleSubmit =() =>{
+  const [admin, setAdmin] = useState({
+    username:'',
+    password:'',
+  })
 
+  const handleChange= (event)=>{
+    const{name, value} = event.target;
+    setAdmin(prevState=>({
+      ...prevState, // 바꾸지 않는 값은 유지
+      [name]: value // 바꾸는 값을 오버라이드
+    }));
+  }
+
+  const handleSubmit = async(event) =>{
+    event.preventDefault();
+    console.log(admin)
+
+    const body = {
+      ...admin
     }
+
+    try{
+      await axiosInstance.post('/admin/login', body);
+      toast.info('Success!')
+    } catch(err){
+      console.error(err);
+    }
+  }
   return (
     <section className="flex flex-col mt-10 my-10 justify-center mt-0 max-w-[400px] m-auto">
       <div className={styles.contact4} ></div>
@@ -12,15 +39,18 @@ const LoginPage = () => {
         <h1 className="text-3xl font-semibold text-center">
           Admin Login
         </h1>
-        <form className="mt-6" onSubmit={handleSubmit()}>
+        <form className="mt-6" onSubmit={handleSubmit}>
           <div className="mb-2">
             <label
-              htmlFor="email"
+              htmlFor="username"
               className="text-sm font-semibold text-gray-800"
-            >Email</label>
+            >Username</label>
             <input
-              type="email"
-              id="email"
+              onChange={handleChange}
+              type="text"
+              id="username"
+              name='username'
+              value={admin.username}
               className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
             />
           </div>
@@ -31,15 +61,18 @@ const LoginPage = () => {
               className="text-sm font-semibold text-gray-800"
             >Password</label>
             <input
+              onChange={handleChange}
               type="password"
               id="password"
+              name='password'
+              value={admin.password}
               className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
             />
           </div>
 
           <div className="mt-6"> 
             <button
-              type="submit" className="w-full bg-black text-white px-4 py-2 rounded-md hover:-bg--medium duration-200 text-base"
+              type="submit" className="w-full -bg--color text-white px-4 py-2 rounded-md hover:-bg--medium duration-200 text-base"
             >
               Login
             </button>
