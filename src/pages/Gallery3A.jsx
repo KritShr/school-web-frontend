9d39121a9d40b2a3dff7b394a3a304c2e5d5a616
kwak <sal091625@gmail.com>
2024-08-11 23:51:54 +0545

    feat: gallery css 수정

--- src/pages/Gallery3A.jsx
+++ src/pages/Gallery3A.jsx
@@ -1,4 +1,3 @@
-import stylesBtn from "../css/components/Button.module.css";
 import styles from "../css/page/Gallery3A.module.css";
 import { useState, useEffect } from "react"
 import SearchInput from "../components1/SearchInput";
@@ -30,6 +29,7 @@ const Gallery3A = () => {
         setGalleries([...galleries, ...response.data.galleries]) 
       } else{
         setGalleries(response.data.galleries);
+        console.log(galleries)
       }
       setHasMore(response.data.hasMore);
       setLoadMore(false);
@@ -49,84 +49,49 @@ const Gallery3A = () => {
     setSearchTerm(event.target.value);
     fetchGalleries(0, limit, loadMore, event.target.value);
   }
-
   
   return (
-    <div className={styles.gallery3a}>
-      <main className={styles.subHeaderParent}>
-        <section className={styles.galleryWrapper}>
-          <div className={styles.gallery4}>
-            <img
-              className={styles.rectangleIcon}
-              alt=""
-              src="/rectangle6.svg"
-            />
-            <div className={styles.image}>
-              <div className={styles.imageChild} />
-              <img className={styles.imageItem} alt="" />
-              <img className={styles.imageInner} alt="" />
-              <div className={styles.overlay}>
-                <div className={styles.overlayChild} />
-                <div className={styles.icon}>
-                  <div className={styles.iconBd} />
-                  <img className={styles.volleyballBallIcon} alt="" />
-                  <div className={styles.div}></div>
-                  <img className={styles.iconChild} alt="" />
-                  <img className={styles.iconItem} alt="" />
-                </div>
-                <div className={styles.text}>
-                  AfLorem ipsum dolor sit amet, consectetur adcing elit.
-                </div>
-              </div>
-            </div>
-            <div className={styles.galleryContainer}>
-              <div className={styles.galleryItems}>
-                <div className={styles.galleryTitleContainer}>
-                  <h3 className={styles.ourGallery}>
-                    <span>{` `}</span>
-                    <span className={styles.our}>Our</span>
-                    <span> Gallery</span>
-                  </h3>
-                  <div className={styles.galleryDivider}>
-                    <div className={styles.titleDivider} />
-                  </div>
-                </div>
-                <div className={styles.imageRow}>
-                  <div className={styles.imagePlaceholder} />
+    <div className="px-10 py-10 sm:px-4 lg:px-40 -bg-white">
+      <div className={styles.contact4}>
+        <div className="justify-end flex">
+          <SearchInput 
+            setSearchTerm = {setSearchTerm}
+            searchTerm = {searchTerm}
+            onSearch = {handleSearchTerm}
+          />
+        </div>
+          
+        {/** gallery 목록 */}
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-3 lg:gap-5 py-10">
+          {galleries.map((gallery) => {
+            const formattedDate = new Date(gallery.updatedAt).toLocaleDateString('en-CA');
+
+            return (
+              <div key={gallery.id} className="flex flex-col">
+                <img
+                  className="w-full h-full object-contain"
+                  alt={gallery.title}
+                  src={`${import.meta.env.VITE_SERVER_URL}/${gallery.image}`}
+                />
+                {/* Adjusted margin-top for tighter spacing */}
+                <div className="flex mb-3 justify-between items-center text-lg text-gray-500">
+                  <span>{gallery.title}</span>
+                  <span>{formattedDate}</span>
                 </div>
               </div>
-            </div>
-            <SearchInput 
-              setSearchTerm = {setSearchTerm}
-              searchTerm = {searchTerm}
-              onSearch = {handleSearchTerm}
-            />
-            <div className={styles.imageCarousel}>
-              <div className={styles.carouselImages}>
-                {galleries.map((gallery)=>(
-                  <div key={gallery.image}>
-                    <img
-                      className={styles.imageIcon10}
-                      alt={gallery.title}
-                      src={`${import.meta.env.VITE_SERVER_URL}/${gallery.image}`}
-                    />
-                  </div>
-                ))}
-                
-              </div>
-              <div className={styles.carouselButtons}>
-                <button
-                  className={[stylesBtn.button].join(" ")}
-                  onClick={handleLoadMore}
-                >
-                  <div className={stylesBtn.txt}>More</div>
-                  <img className={stylesBtn.icon} alt="" src="/vector-1-11.svg" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
+            );
+          })}
+        </div>
+
+        <div className="justify-end flex">
+          <button
+              className="-bg--color-silver text-white px-4 py-2 rounded-md hover:-bg--medium duration-200 text-base"
+              onClick={handleLoadMore}
+            >
+            More
+          </button>
+        </div>
+      </div> 
     </div>
   );
 };
