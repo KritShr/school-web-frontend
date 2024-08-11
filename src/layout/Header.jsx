92724e31845e4bf51691e69aa97b4c4104764ed6
kwak <sal091625@gmail.com>
2024-08-11 22:59:02 +0545

    feat: header 색상 및 굵기 조정

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -17,19 +17,19 @@ const Header = () => {
           className="h-20 w-20"
           loading="lazy"
           alt=""
-          src="/image-3@2x.png"
+          src="/image-3@2x.png"x
         />
-        <h2 className="font-bold text-4xl">Creative Learners' Academy</h2>
+        <h2 className="font-bold text-4xl -text--text-default-default">Creative Learners' Academy</h2>
       </div>
 
-      <div className="flex items-center gap-7">
-        <h3 className={`font-medium text-2xl cursor-pointer hover:-text--medium ${currentPath === '/' ? '-text--medium' : ''}`} onClick={() => moveTo('/')}>Home</h3>
-        <h3 className={`font-medium text-2xl cursor-pointer hover:-text--medium ${currentPath === '/about-us' ? '-text--medium' : ''}`} onClick={() => moveTo('/about-us')}>About Us</h3>
-        <h3 className={`font-medium text-2xl cursor-pointer hover:-text--medium ${currentPath === '/management' ? '-text--medium' : ''}`} onClick={() => moveTo('/management')}>Management</h3>
-        <h3 className={`font-medium text-2xl cursor-pointer hover:-text--medium ${currentPath === '/facilities' ? '-text--medium' : ''}`} onClick={() => moveTo('/facilities')}>Facility</h3>
-        <h3 className={`font-medium text-2xl cursor-pointer hover:-text--medium ${currentPath === '/notice' ? '-text--medium' : ''}`} onClick={() => moveTo('/notice')}>Notice</h3>
-        <h3 className={`font-medium text-2xl cursor-pointer hover:-text--medium ${currentPath === '/gallery' ? '-text--medium' : ''}`} onClick={() => moveTo('/gallery')}>Gallery</h3>
-        <h3 className={`font-medium text-2xl cursor-pointer hover:-text--medium ${currentPath === '/contact' ? '-text--medium' : ''}`} onClick={() => moveTo('/contact')}>Contact Us</h3>
+      <div className="flex items-center gap-7 -text--text-default-default">
+        <h3 className={`font-semibold text-2xl cursor-pointer hover:-text--medium ${currentPath === '/' ? '-text--medium' : ''}`} onClick={() => moveTo('/')}>Home</h3>
+        <h3 className={`font-semibold text-2xl cursor-pointer hover:-text--medium ${currentPath === '/about-us' ? '-text--medium' : ''}`} onClick={() => moveTo('/about-us')}>About Us</h3>
+        <h3 className={`font-semibold text-2xl cursor-pointer hover:-text--medium ${currentPath === '/management' ? '-text--medium' : ''}`} onClick={() => moveTo('/management')}>Management</h3>
+        <h3 className={`font-semibold text-2xl cursor-pointer hover:-text--medium ${currentPath === '/facilities' ? '-text--medium' : ''}`} onClick={() => moveTo('/facilities')}>Facility</h3>
+        <h3 className={`font-semibold text-2xl cursor-pointer hover:-text--medium ${currentPath === '/notice' ? '-text--medium' : ''}`} onClick={() => moveTo('/notice')}>Notice</h3>
+        <h3 className={`font-semibold text-2xl cursor-pointer hover:-text--medium ${currentPath === '/gallery' ? '-text--medium' : ''}`} onClick={() => moveTo('/gallery')}>Gallery</h3>
+        <h3 className={`font-semibold text-2xl cursor-pointer hover:-text--medium ${currentPath === '/contact' ? '-text--medium' : ''}`} onClick={() => moveTo('/contact')}>Contact Us</h3>
         <img
           className="size-auto"
           loading="lazy"
