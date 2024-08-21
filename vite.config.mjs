49fbfe0894e37f6303815cb1c11a083ef98e9ea0
kwak <sal091625@gmail.com>
2024-08-21 23:23:15 +0545

    feat:vite server set

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,5 +11,11 @@ export default defineConfig({
     alias: {
       'nepali-calendar-react': 'nepali-calendar-react/lib/index.js'
     }
+  },
+  server: {
+    host: '0.0.0.0', // 모든 IP에서 접근 가능하도록 설정
+    port: 5173,      // 사용할 포트
   }
 });
+
+
